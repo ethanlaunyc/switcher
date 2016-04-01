8e1ea040c1309d6c1f44c9d723ba393c6a403f40
Henry Wu <henry.wu@xad.com>
2016-04-01 14:30:19 -0700

    add clear

--- switcher/build.sh
+++ switcher/build.sh
@@ -1,3 +1,8 @@
+if [ ! -d "cplusplus_python" ]; then
+    exit
+fi
+pushd cplusplus_python
+
 g++ -c -O2 -fPIC uswitcher.cpp -o uswitcher.o -I/usr/include/python2.7/
 g++ -c -O2 -fPIC switcher.cpp -o switcher.o -I/usr/include/python2.7/
 g++ -shared -O2 -o uswitcher.so uswitcher.o switcher.o -lpython2.7 -lboost_python
@@ -5,3 +10,5 @@ if [ "$?" = "0" ]; then
   cp uswitcher.so ../python/
   rm -fr uswitcher.o switcher.o
 fi
+
+popd
---
+++ switcher/clear.sh
@@ -0,0 +1,3 @@
+find . -name "*\.so" -exec rm -f {} \;
+find . -name "*\.o" -exec rm -f {} \;
+find . -name "*\.pyc" -exec rm -f {} \;
